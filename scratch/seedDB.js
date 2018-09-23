4de0eff6e643bf769047d098c7dc5cfdd0fbd32c
Alfred Law <alfred.lawsn@gmail.com>
2018-09-23 16:41:35 +0000

    discover page working and sorting by descending order, seed file updated

--- scratch/seedDB.js
+++ scratch/seedDB.js
@@ -1,141 +1,125 @@
-INSERT INTO users (id, name, email, password, avatar_url, bio) VALUES
+INSERT INTO users (name, email, password, avatar_url, bio) VALUES
     (
-      '1',
       'Newton123',
       'newton@kumquat.com',
       'asdf',
       'https://78.media.tumblr.com/dbfc0a92ac093f9ca92706667cb3af59/tumblr_otxu3zZnha1wvsgwno6_400.png',
       'I have a doggie named Cat, and he likes to eat ice cream during the winter..'
     );
-INSERT INTO users (id, name, email, password, avatar_url, bio) VALUES
+INSERT INTO users (name, email, password, avatar_url, bio) VALUES
     (
-      '2',
       'Descartes',
       'descartes@kumquat.com',
       'asdf',
       'https://pbs.twimg.com/media/C_ALJSrXsAA6FT2.jpg',
       'I have a doggie named Cat, and he likes to eat ice cream during the winter..'
     );
-INSERT INTO users (id, name, email, password, avatar_url, bio) VALUES
+INSERT INTO users (name, email, password, avatar_url, bio) VALUES
     (
-      '3',
       'Mary_eatsCarrots',
       'mary@kumquat.com',
       'asdf',
       'http://hugomendez.com/wp-content/uploads/2014/05/Profile_square.jpg',
       'I like dogs!'
     );
-INSERT INTO users (id, name, email, password, avatar_url, bio) VALUES
+INSERT INTO users (name, email, password, avatar_url, bio) VALUES
     (
-      '4',
       'Michael',
       'michael@kumquat.com',
       'asdf',
       'https://pbs.twimg.com/media/C_ALJSrXsAA6FT2.jpg',
       'I have a doggie named Cat, and he likes to eat ice cream during the winter..'
     );
-INSERT INTO users (id, name, email, password, avatar_url, bio) VALUES
+INSERT INTO users (name, email, password, avatar_url, bio) VALUES
     (
-      '5',
       'Raymond',
       'raymond@kumquat.com',
       'asdf',
       'http://theflowcentre.com/wp-content/uploads/2018/05/cam-profile-square.jpg',
       'I like long walks on the beach ~'
     );
-INSERT INTO users (id, name, email, password, avatar_url, bio) VALUES
+INSERT INTO users (name, email, password, avatar_url, bio) VALUES
     (
-      '6',
       'Nicola',
       'nicola@kumquat.com',
       'asdf',
       'https://pd.church/wp-content/uploads/2018/06/Sam-Song-Profile-Square.jpg',
       'I have a passion for base jumping and fountain pens'
     );
-INSERT INTO users (id, name, email, password, avatar_url, bio) VALUES
+INSERT INTO users (name, email, password, avatar_url, bio) VALUES
     (
-      '7',
       'Donny',
       'donny@kumquat.com',
       'asdf',
       'http://www.erlin.ca/images/eric-profile-square.jpg',
       'Heavy metal and World of Warcraft!!'
     );
-INSERT INTO users (id, name, email, password, avatar_url, bio) VALUES
+INSERT INTO users (name, email, password, avatar_url, bio) VALUES
     (
-      '8',
       'Patrick',
       'patric@kumquat.com',
       'asdf',
       'http://jbwaggoner.com/wp-content/uploads/2014/08/2014_ProfileSquare800.jpg',
       'Im a huge Spice girls fan'
     );
-INSERT INTO users (id, name, email, password, avatar_url, bio) VALUES
+INSERT INTO users (name, email, password, avatar_url, bio) VALUES
     (
-      '9',
       'Jenna',
       'jenna@kumquat.com',
       'asdf',
       'http://onlineprofitplaybook.com/wp-content/uploads/2016/10/jelena-profile-square-copy.jpg',
       'I eat raw broccoli together with cooked tomatoes'
     );
-INSERT INTO users (id, name, email, password, avatar_url, bio) VALUES
+INSERT INTO users (name, email, password, avatar_url, bio) VALUES
     (
-      '10',
       'Emily',
       'emily@kumquat.com',
       'asdf',
       'https://www.empowervate.org/wp-content/uploads/bb-plugin/cache/Pam-Profile-square.jpg',
       'I will kick your ass in arm wrestling'
     );
-INSERT INTO users (id, name, email, password, avatar_url, bio) VALUES
+INSERT INTO users (name, email, password, avatar_url, bio) VALUES
     (
-      '11',
       'Paula',
       'paula@kumquat.com',
       'asdf',
       'https://houstonswords.com/wp-content/uploads/2018/08/Hayley-Profile-square.jpg',
       'I want to be a professional snowboarder!'
     );
-INSERT INTO users (id, name, email, password, avatar_url, bio) VALUES
+INSERT INTO users (name, email, password, avatar_url, bio) VALUES
     (
-      '12',
       'Lorenzo',
       'lorenzo@kumquat.com',
       'asdf',
       'https://www.exemortgages.co.uk/wp-content/uploads/2018/07/David-Grimshaw-profile-square-640x640.jpg',
       'I am in a professional knitters association'
     );
-INSERT INTO users (id, name, email, password, avatar_url, bio) VALUES
+INSERT INTO users (name, email, password, avatar_url, bio) VALUES
     (
-      '13',
       'Brenda',
       'brenda@kumquat.com',
       'asdf',
       'https://tjcoxforcongress.com/sites/default/files/feinstein_profile_square.jpg',
       'I eat McDonalds for breakfast, lunch, and dinner'
     );
-INSERT INTO users (id, name, email, password, avatar_url, bio) VALUES
+INSERT INTO users (name, email, password, avatar_url, bio) VALUES
     (
-      '14',
       'Rocker126',
       'rocker@kumquat.com',
       'asdf',
       'http://www.urhobosocialclublagos.com/wp-content/uploads/2017/07/default-avatar-ginger-guy.png',
       'I saw the sign, and it opened up my eyes I saw the sign'
     );
-INSERT INTO users (id, name, email, password, avatar_url, bio) VALUES
+INSERT INTO users (name, email, password, avatar_url, bio) VALUES
     (
-      '15',
       'Anonymous56',
       'billy@kumquat.com',
       'asdf',
       'https://eaglepointhomes.com/wp-content/uploads/2018/04/tdawg_square.jpg',
       'When I grow up, I want to be friends with Katy Perry'
     );
-INSERT INTO users (id, name, email, password, avatar_url, bio) VALUES
+INSERT INTO users (name, email, password, avatar_url, bio) VALUES
     (
-      '16',
       'danny',
       'danny@kumquat.com',
       'asdf',
@@ -144,9 +128,8 @@ INSERT INTO users (id, name, email, password, avatar_url, bio) VALUES
     );
 
 
-INSERT INTO resources (id, title, description, url, date_created, picture_url, tags, user_id, rating, likes) VALUES
+INSERT INTO resources (title, description, url, date_created, picture_url, tags, user_id, rating, likes) VALUES
     (
-      '1',
       'Stars are wonderful',
       'This page on astronomy is super useful',
       'http://www.astronomy.com',
@@ -158,9 +141,8 @@ INSERT INTO resources (id, title, description, url, date_created, picture_url, t
       '12'
 
     );
-INSERT INTO resources (id, title, description, url, date_created, picture_url, tags, user_id, rating, likes) VALUES
+INSERT INTO resources (title, description, url, date_created, picture_url, tags, user_id, rating, likes) VALUES
     (
-      '2',
       'Fountain pens are the best pens',
       'Fountain pens are the best at writing calligraphy, and signing your autograph when you are famous',
       'https://www.wonderpens.ca/',
@@ -171,9 +153,8 @@ INSERT INTO resources (id, title, description, url, date_created, picture_url, t
       '4',
       '16'
     );
-INSERT INTO resources (id, title, description, url, date_created, picture_url, tags, user_id, rating, likes) VALUES
+INSERT INTO resources (title, description, url, date_created, picture_url, tags, user_id, rating, likes) VALUES
     (
-      '3',
       'Hiking trails in Toronto',
       'Here are the best hiking trails that I found in and around Toronto',
       'https://www.blogto.com/sports_play/2012/06/hiking_trails_in_and_around_toronto/',
@@ -184,9 +165,8 @@ INSERT INTO resources (id, title, description, url, date_created, picture_url, t
       '3',
       '4'
     );
-INSERT INTO resources (id, title, description, url, date_created, picture_url, tags, user_id, rating, likes) VALUES
+INSERT INTO resources (title, description, url, date_created, picture_url, tags, user_id, rating, likes) VALUES
     (
-      '4',
       'Coding 101',
       'This is the best website to get started on coding',
       'https://developer.mozilla.org/en-US/docs/Learn/Getting_started_with_the_web/JavaScript_basics',
@@ -197,9 +177,8 @@ INSERT INTO resources (id, title, description, url, date_created, picture_url, t
       '4',
       '9'
     );
-INSERT INTO resources (id, title, description, url, date_created, picture_url, tags, user_id, rating, likes) VALUES
+INSERT INTO resources (title, description, url, date_created, picture_url, tags, user_id, rating, likes) VALUES
     (
-      '5',
       'Video games',
       'The ultimate guide to the latest video games',
       'https://guides.gamepressure.com/',
@@ -210,9 +189,8 @@ INSERT INTO resources (id, title, description, url, date_created, picture_url, t
       '5',
       '34'
     );
-INSERT INTO resources (id, title, description, url, date_created, picture_url, tags, user_id, rating, likes) VALUES
+INSERT INTO resources (title, description, url, date_created, picture_url, tags, user_id, rating, likes) VALUES
     (
-      '6',
       'Table Tennis Rocks',
       'The ultimate guide to table tennis in Canada',
       'http://ttcan.ca/',
@@ -223,9 +201,8 @@ INSERT INTO resources (id, title, description, url, date_created, picture_url, t
       '3',
       '12'
     );
-INSERT INTO resources (id, title, description, url, date_created, picture_url, tags, user_id, rating, likes) VALUES
+INSERT INTO resources (title, description, url, date_created, picture_url, tags, user_id, rating, likes) VALUES
     (
-      '7',
       'Learn how to knit',
       'The very best website to learn how to knit, I swear',
       'https://www.loveknitting.com/us/how-to-knit',
@@ -236,22 +213,20 @@ INSERT INTO resources (id, title, description, url, date_created, picture_url, t
       '3',
       '2'
     );
-INSERT INTO resources (id, title, description, url, date_created, picture_url, tags, user_id, rating, likes) VALUES
+INSERT INTO resources (title, description, url, date_created, picture_url, tags, user_id, rating, likes) VALUES
     (
-      '8',
       'Geocaching in Niagara Falls',
       'This is the ultimate resource for geocaching in Niagara Falls!',
       'https://www.niagaraparks.com/visit-niagara-parks/nature-activities/geocaching/',
-      '2019-02-10',
+      '2017-02-10',
       'https://cdn0.wideopenspaces.com/wp-content/uploads/2018/01/geocachingfeature1-630x339.jpg',
       'geocaching',
       '3',
       '5',
       '20'
     );
-INSERT INTO resources (id, title, description, url, date_created, picture_url, tags, user_id, rating, likes) VALUES
+INSERT INTO resources (title, description, url, date_created, picture_url, tags, user_id, rating, likes) VALUES
     (
-      '9',
       'Urban Splunking DUH',
       'Umm hello urban splunking places in and around Milwaukee!',
       'https://radiomilwaukee.org/story/community-stories/spelunking-20-little-milwaukee-landmarks/',
@@ -262,9 +237,8 @@ INSERT INTO resources (id, title, description, url, date_created, picture_url, t
       '3',
       '16'
     );
-INSERT INTO resources (id, title, description, url, date_created, picture_url, tags, user_id, rating, likes) VALUES
+INSERT INTO resources (title, description, url, date_created, picture_url, tags, user_id, rating, likes) VALUES
     (
-      '10',
       'Drone Racing ZOOM ZOOM',
       'This is an awesome article on drone racing',
       'https://www.newyorker.com/magazine/2018/02/05/the-trippy-high-speed-world-of-drone-racing',
@@ -275,11 +249,10 @@ INSERT INTO resources (id, title, description, url, date_created, picture_url, t
       '5',
       '42'
     );
-INSERT INTO resources (id, title, description, url, date_created, picture_url, tags, user_id, rating, likes) VALUES
+INSERT INTO resources (title, description, url, date_created, picture_url, tags, user_id, rating, likes) VALUES
     (
-      '11',
       'Base Jumping!!!',
-      'Don\'t know what base jumping is? This page will teach you',
+      'Dont know what base jumping is? This page will teach you',
       'https://www.tripsavvy.com/what-is-base-jumping-34497',
       '2011-02-10',
       'https://www.tripsavvy.com/thmb/6bMSL-Ge2MROfFdKTFlcCN-5ieY=/950x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/GettyImages-889748872-5b3e43fd46e0fb00375bd5af.jpg',
@@ -288,9 +261,8 @@ INSERT INTO resources (id, title, description, url, date_created, picture_url, t
       '3',
       '12'
     );
-INSERT INTO resources (id, title, description, url, date_created, picture_url, tags, user_id, rating, likes) VALUES
+INSERT INTO resources (title, description, url, date_created, picture_url, tags, user_id, rating, likes) VALUES
     (
-      '12',
       'Ultimage guide on Drones 2018',
       'These are probably the best drones on the market today',
       'https://www.cnet.com/topics/drones/products/',
@@ -301,9 +273,8 @@ INSERT INTO resources (id, title, description, url, date_created, picture_url, t
       '3',
       '11'
     );
-INSERT INTO resources (id, title, description, url, date_created, picture_url, tags, user_id, rating, likes) VALUES
+INSERT INTO resources (title, description, url, date_created, picture_url, tags, user_id, rating, likes) VALUES
     (
-      '13',
       'Agricultural Drones',
       'Farmers are using agricultural drones now as part of their process',
       'https://www.technologyreview.com/s/526491/agricultural-drones/',
